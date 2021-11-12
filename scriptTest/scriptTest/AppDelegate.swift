81b8c8afdb3748d12b8434acec67e085afafafdb
Babitha Shetty K <babitha.shetty@globaldelight.com>
2021-11-12 09:22:43 +0530

    project added

---
+++ scriptTest/scriptTest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  scriptTest
+//
+//  Created by Babitha Shetty K on 12/11/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ scriptTest/scriptTest/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  scriptTest
+//
+//  Created by Babitha Shetty K on 12/11/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
